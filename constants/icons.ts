d91991e1e898de8a6301b9d157a03f560c2fc91c
awaasolution <awaasolution@gmail.com>
2025-04-01 22:54:40 +0630

    added assets and tailwindcss color pallete updated, and fonts added to app.json

---
+++ constants/icons.ts
@@ -0,0 +1,24 @@
+import person from "@/assets/icons/person.png"
+import home from "@/assets/icons/home.png"
+import google from "@/assets/icons/google.png"
+import wallet from "@/assets/icons/wallet.png"
+import rightArrow from "@/assets/icons/right-arrow.png"
+import shield from "@/assets/icons/shield.png"
+import search from "@/assets/icons/search.png"
+import backArrow from "@/assets/icons/back-arrow.png"
+import bell from "@/assets/icons/bell.png"
+import logout from "@/assets/icons/logout.png"
+
+
+export default {
+    person,
+    home,
+    google,
+    wallet,
+    rightArrow,
+    shield,
+    search,
+    backArrow,
+    bell,
+    logout
+}
